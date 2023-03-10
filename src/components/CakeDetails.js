21cd66eb769fa1f1ba88182aa58e7c2266dfb943
Hansine Marshall <hansinemarshall@Hansines-MacBook-Pro.local>
2023-03-10 15:19:37 +0000

    refactored components and container, include state to show total sale value

--- src/components/CakeDetails.js
+++ src/components/CakeDetails.js
@@ -1,68 +1,21 @@
+function CakeDetails({cake,count, onButtonClick}){
 
-let cakeMenu =[
-    {
-        cakeName: "Victoria Sponge",
-           ingredients: [
-               "eggs",
-              "butter",
-              "sugar",
-              "self-raising flour",
-              "baking powder",
-              "milk"
-          ],
-          price: 5,
-            rating: 5
-    },
-    {
-         cakeName: "Tea Loaf",
-           ingredients: [
-               "eggs",
-              "oil",
-              "dried fruit",
-              "sugar",
-              "self-raising flour",
-              "strong tea",
-          ],
-          price: 2,
-          rating: 3
-    },
-    {
-         cakeName: "Carrot Cake",
-           ingredients: [
-            "carrots",
-              "walnuts",
-              "oil",
-              "cream cheese",
-              "flour",
-              "sugar",
-           ],
-           price: 8,
-           rating: 5
-    }      
-]
-
-
-    
-   function CakeDetails(){
-   
-        return(
-            <>
-                {cakeMenu.map((cake, index) => {
-
- 
-                    return (
-                     <div key={index}>
-                        <h3>{cake.cakeName}</h3>
-                        <p>Ingredients: <em>{cake.ingredients.join(', ')}</em></p>
-                        <p>Price: £{cake.price}</p>
-                        <p>Rating: {cake.rating}/5</p>
-                        <button> SELL CAKE</button>
-                     </div>
-                    )
-                })}
-            </>
-        )
+    const handleSellCakeClick = () => {
+        onButtonClick(count+cake.price);
     }
+   
+    return(
+        <>
+        <div>
+             <h3>{cake.cakeName}</h3>
+             <p>Ingredients: <em>{cake.ingredients.join(', ')}</em></p>
+             <p>Price: £{cake.price}</p>
+             <p>Rating: {cake.rating}/5</p>
+             <button onClick = {handleSellCakeClick}> SELL CAKE</button>
+         </div>
+                
+        </>
+    )
+}
 
-    export default CakeDetails;
-    
+export default CakeDetails;
--- src/containers/SellCakeContainer.js
+++ src/containers/SellCakeContainer.js
@@ -3,18 +3,63 @@ import CakeDetails from "../components/CakeDetails";
 
 const SellCakeContainer = () => {
     
-     
-    
+    let cakeMenu =[
+        {
+            cakeName: "Victoria Sponge",
+               ingredients: [
+                   "eggs",
+                  "butter",
+                  "sugar",
+                  "self-raising flour",
+                  "baking powder",
+                  "milk"
+              ],
+              price: 5,
+                rating: 5
+        },
+        {
+             cakeName: "Tea Loaf",
+               ingredients: [
+                   "eggs",
+                  "oil",
+                  "dried fruit",
+                  "sugar",
+                  "self-raising flour",
+                  "strong tea",
+              ],
+              price: 2,
+              rating: 3
+        },
+        {
+             cakeName: "Carrot Cake",
+               ingredients: [
+                "carrots",
+                  "walnuts",
+                  "oil",
+                  "cream cheese",
+                  "flour",
+                  "sugar",
+               ],
+               price: 8,
+               rating: 5
+        }      
+    ]
 
-    
+    const [cakeCount, setCakeCount] = useState(0);
+
 
 
     return(
         <>
             <h2>Cake Menu</h2>
-            <CakeDetails ></CakeDetails>
-
-            <p>Total Sales: £ </p>
+            <>
+                {cakeMenu.map((cake, index) => {
+                    return (
+                     <CakeDetails cake={cake} key={index} count={cakeCount} onButtonClick ={setCakeCount}></CakeDetails>
+                    )
+                })}
+            </>
+            <p>Total Sales: £{cakeCount} </p>
         </>
     )
 }
